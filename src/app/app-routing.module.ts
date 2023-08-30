5768f2779e86706c6c9a7f82b799a72417dde063
Byron Kruger <bkruger@jhb.dvt.co.za>
2023-08-30 23:24:46 +0200

    Added app navigation content

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './src/app/components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
