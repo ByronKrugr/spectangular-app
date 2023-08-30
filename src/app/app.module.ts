d225dbacefeaca245dea1cad16114a073f14d93b
Byron Kruger <bkruger@jhb.dvt.co.za>
2023-08-30 23:24:46 +0200

    Added app navigation content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './src/app/components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
